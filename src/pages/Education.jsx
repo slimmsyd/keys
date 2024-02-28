86ab067664d7c7e562d558983a3e308c2b677a93
Sydney Sanders <ssanderss444@gmail.com>
2024-02-27 19:45:28 -0500

    updated keys site

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -23,16 +23,16 @@ const Education = () => {
           <Header />
           <section className="expertise-container pb-14 px-8 w-full h-auto flex items-left justify-center flex-col">
               <h1 className="w-full pt-[.8em] text-left text-[calc(16px_+_12vw)] text-white font-['Cinzel'] font-bold lg:pt-[.25em]">Education</h1>
-              <div className='flex items-center justify-center flex-col lg:flex-row-reverse lg:justify-between'>
+              <div className='flex items-center justify-center flex-col lg:flex-col lg:justify-between'>
                 <img 
-                  className='max-m-full w-[600px] h-[500px] object-cover rounded-lg border border-[rgba(255,255,255,.2)]'
-                  src={bwoLogo} 
+                className='w-[400px] h-[240px] md:w-[520px] md:h-[300px] lg:w-[100%] lg:h-[860px] pt-16'
+                src={bwoLogo} 
                   alt='The Block World Order'
                 />
-                <div className='w-full py-4 flex items-left justify-center flex-col gap-4 lg:w-1/2 lg:px-8'>
+                <div className='w-full py-4 pt-16 pb-16 flex items-center justify-center flex-col gap-4 lg:w-2/2 lg:px-8'>
                   <small className="text-[calc(10px_+_.25vw)] text-white font-['Source Sans Pro'] font-thin">BLOCK WORLD ORDER</small>
-                  <h3 className="text-left text-[calc(16px_+_2vw)] text-white font-['Cinzel'] font-thin">EMBRACING THE 80% MINDSET, IGNITING THE 20% SKILLSET</h3>
-                  <p className="text-left text-[calc(16px_+_.5vw)] text-white font-['Source Sans Pro'] font-thin">
+                  <h3 className="text-left text-[calc(16px_+_2vw)] text-white font-['Cinzel'] font-thin text-center">EMBRACING THE 80% MINDSET, IGNITING THE 20% SKILLSET</h3>
+                  <p className="text-left text-[calc(16px_+_.5vw)] text-white font-['Source Sans Pro'] font-thin text-center">
                     Unleash your potential by breaking through limitations and acquiring the necessary skills to stay consistently
                     motivated and intensely focused, even when confronted with adversity. Equip yourself with powerful tools of
                     persuasion, inspiration, and authentic connection, enabling you to become the master of your own destiny.
--- src/pages/HighLevelConversation.jsx
+++ src/pages/HighLevelConversation.jsx
@@ -20,26 +20,26 @@ const HighLevelConversation = () => {
     <>
       <main 
       ref={hlcSection} 
-      className="w-full h-auto px-8 flex items-left justify-center flex-col">
+      className="w-full h-auto px-8 flex items-center justify-center flex-col">
           <Header />
-          <section className="expertise-container pb-14 px-8 w-full h-auto flex items-left justify-center flex-col">
+          <section className="expertise-container pb-14 px-8 w-full h-auto flex items-center justify-center flex-col">
               <h1 className="w-full pt-[1.25em] text-left text-[calc(16px_+_8vw)] leading-[1em] text-white font-['Cinzel'] font-bold lg:pt-[.75em]">High Level Conversations</h1>
-              <div className='py-[4em] flex items-left justify-center flex-col lg:flex-row-reverse lg:justify-between'>
+              <div className='py-[4em] flex items-center justify-center flex-col lg:flex-col lg:justify-between '>
                 {/* <img 
                   className='max-m-full w-[600px] h-[500px] object-cover rounded-lg border border-[rgba(255,255,255,.125)]'
                   src={keysHLC} 
                   alt='High Level Conversations'
                 /> */}
                 <iframe 
-                className='w-[400px] h-[240px] md:w-[520px] md:h-[300px] lg:w-[600px] lg:h-[360px]'
+                className='w-[400px] h-[240px] md:w-[520px] md:h-[300px] lg:w-[100%] lg:h-[860px] pt-16'
                 src="https://www.youtube.com/embed/BesRjzDOYYM" 
                 title="YouTube video player" 
                 frameborder="0" 
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                <div className='w-full py-4 flex items-left justify-center flex-col gap-4 lg:w-1/2 lg:px-8'>
+                <div className='w-full py-4 pt-16 pb-16   flex items-center justify-center flex-col gap-4 lg:w-2/2 lg:px-8'>
                   <small className="text-[calc(10px_+_.25vw)] text-white font-['Source Sans Pro'] font-thin">THE HIGHEST THOUGHTS</small>
                   <h3 className="text-left text-[calc(16px_+_2vw)] text-white font-['Cinzel'] font-thin">ENLIGHTENING THOUGHT PATTERNS WITH 19 KEYS.</h3>
-                  <p className="text-left text-[calc(16px_+_.5vw)] text-white font-['Source Sans Pro'] font-thin">
+                  <p className="text-left text-[calc(16px_+_.5vw)] text-white font-['Source Sans Pro'] font-thin text-center">
                     Embrace the essence of life's journey, not just the destination. Immerse yourself in these transformative keys,
                     empowering you in spirituality, entrepreneurship, future building, and mind management. Let these
                     insights guide and elevate you throughout your profound voyage of growth and self-discovery.
